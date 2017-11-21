17cd7a6241ec5b3cff5d00d884768e91a2c81000
vilkoz <vilko.a@gmail.com>
2017-11-22 00:25:50 +0200

    fix norme

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,7 +6,7 @@
 /*   By: vrybalko <vrybalko@student.unit.ua>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/22 14:30:44 by vrybalko          #+#    #+#             */
-/*   Updated: 2017/09/22 14:32:30 by vrybalko         ###   ########.fr       */
+/*   Updated: 2017/11/21 23:59:08 by vrybalko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,29 @@ static char		**add_to_env(char ***env, char *key, char *value)
 {
 	int		size;
 	int		i;
-	char	**new_env;
 	char	*tmp_key;
+	char	*tmp;
 
 	size = -1;
 	while ((*env)[++size])
 		;
-	new_env = (char **)malloc(sizeof(char*) * (size + 2));
+	*env = (char**)ft_realloc((void*)*env, sizeof(char*) * (size + 1),
+			sizeof(char*) * (size + 2));
 	i = -1;
-	while (++i < size)
-	{
-		new_env[i] = ft_strdup((*env)[i]);
-		ft_strdel(&((*env)[i]));
-	}
+	while (++i < size
+		&& ft_strncmp(key, (*env)[i], ft_strlen_chr((*env)[i], "=")) > 0)
+		;
+	tmp = (*env)[i];
 	tmp_key = ft_strjoin(key, "=");
-	new_env[i++] = ft_strjoin(tmp_key, value);
+	(*env)[i] = ft_strjoin(tmp_key, value);
 	ft_strdel(&tmp_key);
-	new_env[i] = NULL;
-	ft_memdel((void*)(env));
-	return (new_env);
+	while (++i < size + 1)
+	{
+		tmp_key = (*env)[i];
+		(*env)[i] = tmp;
+		tmp = tmp_key;
+	}
+	return (*env);
 }
 
 void			minishell_setenv(int argc, char **argv, char ***env)
@@ -43,7 +47,10 @@ void			minishell_setenv(int argc, char **argv, char ***env)
 	char	*value;
 
 	if (argc != 3)
+	{
+		ft_putendl_fd("usage: setenv key value", 2);
 		return ;
+	}
 	key = argv[1];
 	value = argv[2];
 	env_set(key, value, env);
@@ -54,8 +61,16 @@ void			minishell_getenv(int argc, char **argv, char ***env)
 	char	*value;
 
 	if (argc != 2)
+	{
+		ft_putendl_fd("usage: getenv key", 2);
 		return ;
+	}
 	value = env_get(argv[1], *env);
+	if (value == NULL)
+	{
+		ft_putendl_fd("no such key!", 2);
+		return ;
+	}
 	ft_putendl(value);
 	ft_strdel(&value);
 }
